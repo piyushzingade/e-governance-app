ffd68f1379c0155e3b9e3b73f73e73ccf118e741
Piyushzingade <piyushzingade@gmail.com>
2025-01-16 19:18:57 +0530

    Updated

--- src/components/StudentAppbar.tsx
+++ src/components/StudentAppbar.tsx
@@ -42,7 +42,7 @@ const StudentAppbar = () => {
             "Department",
             "Student",
             "Contact Us",
-            "Practical",
+            
           ].map((item, index) => (
             <div
               key={index}
