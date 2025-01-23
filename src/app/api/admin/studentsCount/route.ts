2a26dd1a393f9ee8883cebef56586dd8fe157d5d
Piyushzingade <piyushzingade@gmail.com>
2025-01-23 16:25:24 +0530

    Updated studentCount route

--- src/app/api/admin/studentsCount/route.ts
+++ src/app/api/admin/studentsCount/route.ts
@@ -1,24 +1,18 @@
 import { connectToDB } from "@/db/mongo";
 import Student from "@/models/Student_Registration";
-import {  NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    await connectToDB();
+    const studentCount = await Student.countDocuments({}).exec();
 
-export default async function GET() {
-    try {
-        await connectToDB();
-        const studentCount = await Student.countDocuments({}).exec();
-
-        return NextResponse.json({
-            studentCount
-        } , {
-            status: 201
-        })
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({
-            error : "Error in Student count route"
-        } , {
-            status: 402
-        })
-    }
+    return NextResponse.json({ studentCount }, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Error in Student count route" },
+      { status: 500 }
+    );
+  }
 }
